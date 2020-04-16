6646e3e40d63b5609ca451431620ee4bd3ffd6e9
Yusuke Takeuchi <v.takeuchi+gh@gmail.com>
2020-04-17 00:55:02 +0900

    feat: add a weapon and particles

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            var sword = GameObject.Find("Sword");
+            float z = sword.transform.position.z;
+            var posClicked = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            posClicked.z = z;
+            sword.transform.position = posClicked;
+        }
+    }
+}
